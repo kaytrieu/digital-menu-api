9e2684a1eeb197d8362771f30fbf38f105254369
Triệu Minh Huy (K13) <huytmse130336@fpt.edu.vn>
2020-09-21 13:19:17 +0700

    add swagger into project

--- digital-menu-api/Startup.cs
+++ digital-menu-api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace digital_menu_api
 {
@@ -25,16 +26,31 @@ namespace digital_menu_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            int apiVersion = Configuration.GetValue<int>("Version");
+
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v" + apiVersion, new OpenApiInfo { Title = "Digital Menu API", Version = "v" + apiVersion });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            int apiVersion = Configuration.GetValue<int>("Version");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v" + apiVersion + "/swagger.json", "Feedback System API");
+            });
 
             app.UseHttpsRedirection();
 
